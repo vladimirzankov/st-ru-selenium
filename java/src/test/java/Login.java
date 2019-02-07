0552fe09be2c347dcd09303f17868923663c0f82
Vladimir Zankov <vladimirzankov@outlook.com>
2019-02-07 20:33:06 +0300

    Basic login scenario

--- java/src/test/java/Login.java
+++ java/src/test/java/Login.java
@@ -1,11 +1,12 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class FirstTest {
+public class Login {
 
 	public WebDriver driver;
 	public WebDriverWait wait;
@@ -18,7 +19,10 @@ public class FirstTest {
 
 	@Test
 	public void firstTest() {
-		driver.get("http://www.google.com");
+		driver.get("http://localhost:8088/litecart/admin/");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name("login")).click();
 	}
 
 	@After
