3144079c608f16cd6d439ae670ff208dbe5fd2d9
Vladimir Zankov <vladimirzankov@outlook.com>
2019-02-16 13:32:08 +0300

    Added tests for order of countries and geo zones

--- java/src/test/java/Tests.java
+++ java/src/test/java/Tests.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Tests {
 
@@ -17,7 +19,7 @@ public class Tests {
 	public void start() {
 			driver = new ChromeDriver();
 			wait = new WebDriverWait(driver, 10);
-		}
+	}
 
 	@Test
 	public void sidebarTest() {
@@ -45,6 +47,63 @@ public class Tests {
 		driver.get("http://localhost:8088/litecart");
 		for(WebElement e : driver.findElements(By.className("product"))) {
 			Assert.assertTrue(e.findElements(By.className("sticker")).size() == 1);
+		}
+	}
+
+	@Test
+	public void countriesOrderTest() {
+		driver.get("http://localhost:8088/litecart/admin/");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name("login")).click();
+		driver.get("http://localhost:8088/litecart/admin/?app=countries&doc=countries");
+
+		ArrayList<String> countries = new ArrayList<>();
+		for(WebElement element : driver.findElements(By.className("row"))) {
+			countries.add(element.findElement(By.tagName("a")).getText());
+		}
+		ArrayList sortedCountries = new ArrayList(countries);
+		Collections.sort(sortedCountries);
+		Assert.assertTrue(countries.equals(sortedCountries));
+
+		for(String s : countries) {
+			WebElement row = driver.findElement(By.xpath("//tr[@class='row'][.//a[contains(., \"" + 	s + "\")]]"));
+			if(Integer.parseInt(row.findElements(By.tagName("td")).get(5).getText()) > 0) {
+				row.findElements(By.tagName("a")).get(1).click();
+				ArrayList<String> zones = new ArrayList<String>();
+				for(WebElement elem : driver.findElements(By.xpath("//table[@id='table-zones']//tr[.//a[@id='remove-zone']]"))) {
+					zones.add(elem.findElement(By.xpath("//td[.//input[contains(@name, 'name')]]")).getText());
+				}
+				ArrayList sortedZones = new ArrayList(zones);
+				Collections.sort(sortedZones);
+				Assert.assertTrue(zones.equals(sortedZones));
+				driver.navigate().back();
+			}
+		}
+	}
+
+	@Test
+	public void geoZonesOrderTest() {
+		driver.get("http://localhost:8088/litecart/admin/");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name("login")).click();
+		driver.get("http://localhost:8088/litecart/admin/?app=geo_zones&doc=geo_zones");
+
+		int geoZonesCount = driver.findElements(By.className("row")).size();
+		for(int i = 0; i < geoZonesCount; i++) {
+			WebElement row = driver.findElements(By.className("row")).get(i);
+			row.findElement(By.cssSelector("a[title=Edit")).click();
+
+			ArrayList<String> zones = new ArrayList<String>();
+			for(WebElement elem : driver.findElements(By.xpath("//table[@id='table-zones']//tr[.//a[@id='remove-zone']]"))) {
+				zones.add(elem.findElement(By.cssSelector("select[name*=zone_code] option[selected]")).getText());
+				System.out.println(elem.findElement(By.cssSelector("select[name*=zone_code] option[selected]")).getText());
+			}
+			ArrayList sortedZones = new ArrayList(zones);
+			Collections.sort(sortedZones);
+			Assert.assertTrue(zones.equals(sortedZones));
+			driver.navigate().back();
 		}
 	}
 
