3befd59dc10ef7804b8cc1de1e42ddd57d6058d1
Vladimir Zankov <vladimirzankov@outlook.com>
2019-02-23 12:58:29 +0300

    Added tests for sugnup, product creation and working with cart

--- java/src/test/java/Tests.java
+++ java/src/test/java/Tests.java
@@ -2,13 +2,14 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.File;
 import java.util.*;
 
 public class Tests {
@@ -99,7 +100,6 @@ public class Tests {
 			ArrayList<String> zones = new ArrayList<String>();
 			for(WebElement elem : driver.findElements(By.xpath("//table[@id='table-zones']//tr[.//a[@id='remove-zone']]"))) {
 				zones.add(elem.findElement(By.cssSelector("select[name*=zone_code] option[selected]")).getText());
-				System.out.println(elem.findElement(By.cssSelector("select[name*=zone_code] option[selected]")).getText());
 			}
 			ArrayList sortedZones = new ArrayList(zones);
 			Collections.sort(sortedZones);
@@ -149,6 +149,88 @@ public class Tests {
 			Assert.assertTrue(info.isCampaignPriceRed());
 			Assert.assertTrue(info.isCampaignPriceBold(driver));
 			Assert.assertTrue(info.campaignPriceFontSize > info.regularPriceFontSize);
+		}
+	}
+
+	@Test
+	public void signUpTest() {
+		driver.get("http://localhost:8088/litecart");
+		driver.findElement(By.cssSelector("[name=login_form] a")).click();
+		driver.findElement(By.name("firstname")).sendKeys("firstname");
+		driver.findElement(By.name("lastname")).sendKeys("lastname");
+		driver.findElement(By.name("address1")).sendKeys("address1");
+		driver.findElement(By.name("postcode")).sendKeys("12345");
+		driver.findElement(By.name("city")).sendKeys("city");
+		Select select = new Select(driver.findElement(By.cssSelector("select[name=country_code]")));
+		((JavascriptExecutor)driver).executeScript("arguments[0].value = \"US\"; arguments[0].dispatchEvent(new Event('change'))", select);
+		String email = System.currentTimeMillis() + "@test.ru";
+		driver.findElement(By.name("email")).sendKeys(email);
+		driver.findElement(By.name("phone")).sendKeys("205-297-5716");
+		driver.findElement(By.name("password")).sendKeys("password");
+		driver.findElement(By.name("confirmed_password")).sendKeys("password");
+		driver.findElement(By.name("create_account")).click();
+		driver.findElement(By.xpath("//a[contains(., 'Logout')]")).click();
+		driver.findElement(By.name("email")).sendKeys(email);
+		driver.findElement(By.name("password")).sendKeys("password");
+		driver.findElement(By.name("login")).click();
+		driver.findElement(By.xpath("//a[contains(., 'Logout')]")).click();
+	}
+
+	@Test
+	public void productCreationTest() {
+		driver.get("http://localhost:8088/litecart/admin/");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name("login")).click();
+		driver.findElement(By.xpath("//span[contains(., 'Catalog')]")).click();
+		driver.findElement(By.xpath("//a[contains(., ' Add New Product')]")).click();
+		driver.findElement(By.xpath("//label[contains(., ' Enabled')]/input[@value=1]")).click();
+		driver.findElement(By.name("name[en]")).sendKeys("Skeleton Duck");
+		driver.findElement(By.name("code")).sendKeys("rd006");
+		driver.findElement(By.cssSelector("input[name='categories[]'][data-name='Root']")).click();
+		driver.findElement(By.cssSelector("input[name='categories[]'][data-name='Rubber Ducks']")).click();
+		driver.findElement(By.cssSelector("input[name='product_groups[]'][value='1-3']")).click();
+		driver.findElement(By.name("quantity")).sendKeys("3");;
+		Select select = new Select(driver.findElement(By.name("sold_out_status_id")));
+		((JavascriptExecutor)driver).executeScript("arguments[0].value = 2; arguments[0].dispatchEvent(new Event('change'))", select);
+		File file = new File("skeleton-rubber-duck.jpg");
+		driver.findElement(By.name("new_images[]")).sendKeys(file.getAbsolutePath());
+		driver.findElement(By.name("date_valid_from")).sendKeys("22022019");
+		driver.findElement(By.name("date_valid_to")).sendKeys("22022020");
+		driver.findElement(By.xpath("//a[contains(., 'Information')]")).click();
+		select = new Select(driver.findElement(By.name("manufacturer_id")));
+		((JavascriptExecutor)driver).executeScript("arguments[0].value = 1; arguments[0].dispatchEvent(new Event('change'))", select);
+		String shortDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin ante massa, eget ornare libero porta congue.";
+		String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin ante massa, eget ornare libero porta congue. Cras scelerisque dui non consequat sollicitudin. Sed pretium tortor ac auctor molestie. Nulla facilisi. Maecenas pulvinar nibh vitae lectus vehicula semper. Donec et aliquet velit. Curabitur non ullamcorper mauris. In hac habitasse platea dictumst. Phasellus ut pretium justo, sit amet bibendum urna. Maecenas sit amet arcu pulvinar, facilisis quam at, viverra nisi. Morbi sit amet adipiscing ante. Integer imperdiet volutpat ante, sed venenatis urna volutpat a. Proin justo massa, convallis vitae consectetur sit amet, facilisis id libero.";
+		driver.findElement(By.name("short_description[en]")).sendKeys(shortDescription);
+		driver.findElement(By.className("trumbowyg-editor")).sendKeys(description);
+		driver.findElement(By.xpath("//a[contains(., 'Prices')]")).click();
+		driver.findElement(By.name("purchase_price")).sendKeys("1");
+		driver.findElement(By.name("prices[USD]")).sendKeys("20.0000");
+		driver.findElement(By.name("prices[EUR]")).sendKeys("0.0000");
+		driver.findElement(By.name("save")).click();
+	}
+
+	@Test
+	public void basicCartTest() {
+		for (int i = 0; i < 3; i++) {
+			driver.get("http://localhost:8088/litecart");
+			driver.findElement(By.className("product")).click();
+			if(driver.findElements(By.name("options[Size]")).size() > 0) {
+				Select select = new Select(driver.findElement(By.name("options[Size]")));
+				((JavascriptExecutor)driver).executeScript("arguments[0].value = 'Medium'; arguments[0].dispatchEvent(new Event('change'))", select);
+			}
+			WebElement currentQuantity = driver.findElement(By.className("quantity"));
+			int fixedQuantity = Integer.parseInt(currentQuantity.getText());
+			driver.findElement(By.name("add_cart_product")).click();
+			wait.until(d -> Integer.parseInt(currentQuantity.getText()) > fixedQuantity);
+			driver.findElement(By.id("logotype-wrapper")).click();
+		}
+		driver.findElement(By.xpath("//a[contains(., 'Checkout')]")).click();
+		List<WebElement> rows;
+		while((rows = driver.findElements(By.cssSelector(".dataTable td"))).size() > 0) {
+			driver.findElement(By.name("remove_cart_item")).click();
+			wait.until(ExpectedConditions.stalenessOf(rows.get(1)));
 		}
 	}
 
